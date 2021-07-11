d661e58eb72d5e49d6c363f767e536402c01ec43
yodev <yodev2177@gmail.com>
2021-07-11 20:47:35 +0900

    Vueインストール

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,17 @@
 const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
 
+environment.plugins.prepend(
+    'VueLoaderPlugin',
+    new VueLoaderPlugin()
+)
+environment.loaders.prepend('vue', {
+    test: /\.vue$/,
+    use: [{
+        loader: 'vue-loader'
+    }]
+})
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
 module.exports = environment
